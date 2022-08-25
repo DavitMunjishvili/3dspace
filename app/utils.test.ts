2a744e4aaeae74d760ca79c888b70eabd800380a
Drummer3 <drummerthree@gmail.com>
2022-08-25 19:23:38 +0400

    validatePhone tests

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,4 +1,4 @@
-import { validateEmail } from "./utils";
+import { validateEmail, validatePhone } from "./utils";
 
 test("validateEmail returns false for non-emails", () => {
   expect(validateEmail(undefined)).toBe(false);
@@ -10,4 +10,17 @@ test("validateEmail returns false for non-emails", () => {
 
 test("validateEmail returns true for emails", () => {
   expect(validateEmail("kody@example.com")).toBe(true);
+});
+
+test("validatePhone returns false for non-phones", () => {
+  expect(validatePhone(undefined)).toBe(false);
+  expect(validatePhone(null)).toBe(false);
+  expect(validatePhone("")).toBe(false);
+  expect(validatePhone("555abcdef")).toBe(false);
+  expect(validatePhone("155112233")).toBe(false);
+  expect(validatePhone("+995555112233")).toBe(false);
+});
+
+test("validatePhone returns true for phones", () => {
+  expect(validatePhone("555112233")).toBe(true);
 });
