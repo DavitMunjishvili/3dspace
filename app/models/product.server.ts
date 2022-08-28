7a1c5398f595233ec35221c3b18c1f9b438c6dac
Drummer3 <drummerthree@gmail.com>
2022-08-28 15:45:13 +0400

    await product prisma function, add updateProduct function

--- app/models/product.server.ts
+++ app/models/product.server.ts
@@ -13,21 +13,30 @@ export type Product = {
 };
 
 export async function getProductById(id: Product["id"]) {
-  return prisma.product.findUnique({ where: { id } });
+  return await prisma.product.findUnique({ where: { id } });
 }
 
 export async function getAllProduct() {
-  return prisma.product.findMany();
+  return await prisma.product.findMany();
 }
 
 export async function getFilters() {
-  const products = prisma.product.findMany({ select: { categories: true } });
+  const products = await prisma.product.findMany({
+    select: { categories: true },
+  });
   let categories: string[] = [];
   (await products).map((product) => {
     return categories.push(...product.categories);
   });
   categories = [...new Set(categories)];
   return { categories };
+}
+
+export async function getEveryPossibleCategory() {
+  // TODO this needs to be distinct
+  // probably pass column name and filter received data
+  // ! note that categories is an array item
+  return await getFilters();
 }
 
 export async function addNewProduct(
@@ -38,7 +47,7 @@ export async function addNewProduct(
   categories: Product["categories"],
   archive: Product["archive"]
 ) {
-  return prisma.product.create({
+  return await prisma.product.create({
     data: {
       name,
       description,
@@ -50,9 +59,26 @@ export async function addNewProduct(
   });
 }
 
-export async function getEveryPossibleCategory() {
-  // TODO this needs to be distinct
-  // probably pass column name and filter received data
-  // ! note that categories is an array item
-  return await getFilters();
+export async function updateProduct(
+  id: Product["id"],
+  name: Product["name"],
+  description: Product["description"],
+  originalPrice: Product["originalPrice"],
+  currentPrice: Product["currentPrice"],
+  categories: Product["categories"],
+  archive: Product["archive"]
+) {
+  return prisma.product.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      description,
+      originalPrice,
+      currentPrice,
+      categories,
+      archive,
+    },
+  });
 }
