46f969ae9bbd3f107d1d9c0ba1fa59e41d23887c
Data Munjishvili <data.munjishvili@pulsar.ai>
2022-08-29 17:34:05 +0400

    fix import type

--- app/session.server.ts
+++ app/session.server.ts
@@ -3,7 +3,7 @@ import invariant from "tiny-invariant";
 import { URL } from "url";
 import { roleMap } from "~/roleMap";
 
-import type { User } from "~/models/user.server";
+import type { User } from "@prisma/client";
 import { getUserById } from "~/models/user.server";
 
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
