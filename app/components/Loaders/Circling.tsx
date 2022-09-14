e6f3193318c885cd3bb863a6c2dd826a53662e04
Data Munjishvili <data.munjishvili@pulsar.ai>
2022-09-14 19:18:11 +0400

    loader

---
+++ app/components/Loaders/Circling.tsx
@@ -0,0 +1,7 @@
+export default function CirclingLoader() {
+  return (
+    <div className="flex p-4">
+      <div className="animate-spin rounded-full border-2 border-b-4 border-indigo-100 border-b-indigo-900 p-4"></div>
+    </div>
+  );
+}
