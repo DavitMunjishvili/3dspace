842e5c54ffdbac1ffdae5f48f8db6f2640fb4aac
Drummer3 <drummerthree@gmail.com>
2022-09-16 22:58:29 +0400

    basic cart quantity badge

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,26 @@
 import { Transition } from "@headlessui/react";
 import { Form, Link } from "@remix-run/react";
-import { useState } from "react";
-import { useOptionalUser } from "~/utils";
+import { useMemo, useState } from "react";
+import { useLocalCart, useOptionalUser } from "~/utils";
 
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
+  const [cartQuantity, setCartQuantity] = useState(0);
   const user = useOptionalUser();
+  const { localCart } = useLocalCart();
+  useMemo(() => {
+    setCartQuantity(
+      Object.keys(localCart).reduce(
+        (accumulator, key) =>
+          accumulator +
+          localCart[key].reduce(
+            (itemQuantity, e) => itemQuantity + e.quantity,
+            0
+          ),
+        0
+      )
+    );
+  }, [localCart]);
 
   return (
     <>
@@ -92,7 +107,7 @@ export default function Header() {
             )}
             <Link
               to="/cart"
-              className="rounded-sm px-2 py-1 duration-150 hover:bg-indigo-100"
+              className="relative rounded-sm px-2 py-1 duration-150 hover:bg-indigo-100"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -108,6 +123,11 @@ export default function Header() {
                   d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
                 />
               </svg>
+              {cartQuantity ? (
+                <span className="absolute top-0 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 p-1 text-xs text-red-50">
+                  {cartQuantity}
+                </span>
+              ) : null}
             </Link>
           </div>
 
