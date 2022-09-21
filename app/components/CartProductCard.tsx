86cd48e8c967a0ea093e0e632313588b2009f0f5
Drummer3 <drummerthree@gmail.com>
2022-09-21 16:45:31 +0400

    improve cart page performance

--- app/components/CartProductCard.tsx
+++ app/components/CartProductCard.tsx
@@ -1,57 +1,39 @@
 import { Link } from "@remix-run/react";
-import { useEffect, useState } from "react";
-import type { Product } from "@prisma/client";
-
 import { generateProductColor } from "~/utils";
-import CirclingLoader from "./Loaders/Circling";
+
+import type { Product } from "@prisma/client";
 import type { CartType } from "~/utils";
 
 export default function CartProductCard({
   id,
+  name,
+  image,
+  originalPrice,
+  currentPrice,
   size,
   color,
   quantity,
 }: {
   id: Product["id"];
+  name: CartType[0]["name"];
+  image: CartType[0]["image"];
+  originalPrice: CartType[0]["originalPrice"];
+  currentPrice: CartType[0]["currentPrice"];
   size: CartType[0]["size"];
   color: CartType[0]["color"];
   quantity: CartType[0]["quantity"];
 }) {
-  const [image, setImage] = useState<File>();
-  const [details, setDetails] = useState<Product>();
-
-  useEffect(() => {
-    // Get Product Image
-    fetch(`/products/${id}/image`)
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw response.statusText;
-      })
-      .then((data) => setImage(data))
-      .catch((error) => console.error("ERROR", error));
-
-    // Get Product Details
-    fetch(`/products/${id}/details`)
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw response.statusText;
-      })
-      .then((data) => setDetails(data))
-      .catch((error) => console.error("ERROR", error));
-  }, [id]);
-
-  if (!details) return <CirclingLoader />;
   return (
     <div className="flex items-center p-4">
       <img
         src={`data:image/jpeg;base64,${image}`}
         className="w-48 rounded-lg"
-        alt={id}
+        alt={name}
       />
       <div className="flex flex-1 justify-between px-4">
         <div>
           <Link to={`/products/${id}`} className="text-2xl font-bold">
-            {details?.name}
+            {name}
           </Link>
           <div className="mt-4 flex flex-col gap-4">
             <label htmlFor="quantity" className="flex items-center gap-2">
@@ -87,15 +69,15 @@ export default function CartProductCard({
           </div>
         </div>
         <p className="text-xl font-bold">
-          {details?.currentPrice ? (
+          {currentPrice ? (
             <>
-              {details?.currentPrice + "₾"}{" "}
+              {currentPrice + "₾"}{" "}
               <span className="text-sm font-normal text-red-500 line-through">
-                {details?.originalPrice + "₾"}
+                {originalPrice + "₾"}
               </span>
             </>
           ) : (
-            details?.originalPrice + "₾"
+            originalPrice + "₾"
           )}
         </p>
       </div>
--- app/routes/cart/index.tsx
+++ app/routes/cart/index.tsx
@@ -5,29 +5,47 @@ import { json, redirect } from "@remix-run/server-runtime";
 import CartProductCard from "~/components/CartProductCard";
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import type { CartType } from "~/utils";
+import { getProductThumbnail } from "~/filesystem.server";
 
 export async function loader({ request }: LoaderArgs) {
   const user = await getUser(request);
   if (!user) return redirect("/");
-  return json(JSON.parse(user.cart) as CartType);
+  const cart = JSON.parse(user.cart || "[]") as CartType;
+  if (cart.length)
+    cart.map(async (item) => {
+      const image = getProductThumbnail(item.productId);
+      item.image = image?.image;
+      return item;
+    });
+  return json(cart);
 }
 
 export default function Cart() {
   const userCart = useLoaderData<typeof loader>();
-
   return (
     <main className="min-h-screen bg-indigo-50 p-4">
       <div className="mx-auto mt-8 flex w-full max-w-7xl justify-between gap-8 rounded-xl border border-indigo-900 ">
         <div className="flex w-2/3 flex-col gap-8">
-          {userCart.map((item) => (
-            <CartProductCard
-              key={item.productId + item.color + item.size}
-              id={item.productId}
-              size={item.size}
-              color={item.color}
-              quantity={item.quantity}
-            />
-          ))}
+          {userCart.length === 0 ? (
+            // TODO Better Empty Cart Message
+            <h1>Empty</h1>
+          ) : (
+            <>
+              {userCart.map((item) => (
+                <CartProductCard
+                  key={item.productId + item.color + item.size}
+                  id={item.productId}
+                  name={item.name}
+                  image={item.image}
+                  originalPrice={item.originalPrice}
+                  currentPrice={item.currentPrice}
+                  size={item.size}
+                  color={item.color}
+                  quantity={item.quantity}
+                />
+              ))}
+            </>
+          )}
         </div>
         <div className="my-8 flex w-1/3 items-start justify-center border-l border-l-indigo-900/50 p-4">
           <div>
