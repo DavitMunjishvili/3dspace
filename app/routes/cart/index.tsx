4872cb619e7e4ef74e3db353adb059f46eb8d4a7
Drummer3 <drummerthree@gmail.com>
2022-09-21 18:16:55 +0400

    improve readibility

--- app/routes/cart/index.tsx
+++ app/routes/cart/index.tsx
@@ -1,11 +1,12 @@
 import { getUser } from "~/session.server";
 import { Link, useLoaderData } from "@remix-run/react";
 import { json, redirect } from "@remix-run/server-runtime";
-
 import CartProductCard from "~/components/CartProductCard";
+import { getProductThumbnail } from "~/filesystem.server";
+import CheckoutCard from "~/components/CheckoutCard";
+
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import type { CartType } from "~/utils";
-import { getProductThumbnail } from "~/filesystem.server";
 
 export async function loader({ request }: LoaderArgs) {
   const user = await getUser(request);
@@ -21,17 +22,17 @@ export async function loader({ request }: LoaderArgs) {
 }
 
 export default function Cart() {
-  const userCart = useLoaderData<typeof loader>();
+  const cart = useLoaderData<typeof loader>();
   return (
     <main className="min-h-screen bg-indigo-50 p-4">
       <div className="mx-auto mt-8 flex w-full max-w-7xl justify-between gap-8 rounded-xl border border-indigo-900 ">
         <div className="flex w-2/3 flex-col gap-8">
-          {userCart.length === 0 ? (
+          {cart.length === 0 ? (
             // TODO Better Empty Cart Message
             <h1>Empty</h1>
           ) : (
             <>
-              {userCart.map((item) => (
+              {cart.map((item) => (
                 <CartProductCard
                   key={item.productId + item.color + item.size}
                   id={item.productId}
@@ -49,7 +50,7 @@ export default function Cart() {
         </div>
         <div className="my-8 flex w-1/3 items-start justify-center border-l border-l-indigo-900/50 p-4">
           <div>
-            {/* <CheckoutCard checkoutCardData={checkoutCardData} /> */}
+            <CheckoutCard cart={cart} />
             <Link
               to="/checkout"
               className="block w-full cursor-pointer rounded-lg bg-indigo-500 py-2 text-center text-indigo-50 shadow-md duration-150 hover:bg-indigo-600"
