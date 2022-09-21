8a2dddad9ba77151f2f3ce1ecf8b670d6c5cd4cd
Drummer3 <drummerthree@gmail.com>
2022-09-21 13:42:56 +0400

    cart for only logged users

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -88,6 +88,30 @@ export default function Header() {
                     {user.name}, Logout
                   </button>
                 </Form>
+                <Link
+                  to="/cart"
+                  className="relative rounded-sm px-2 py-1 duration-150 hover:bg-indigo-100"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                    />
+                  </svg>
+                  {cartQuantity ? (
+                    <span className="absolute top-0 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 p-1 text-xs text-red-50">
+                      {cartQuantity}
+                    </span>
+                  ) : null}
+                </Link>
               </>
             ) : (
               <>
@@ -105,30 +129,6 @@ export default function Header() {
                 </Link>
               </>
             )}
-            <Link
-              to="/cart"
-              className="relative rounded-sm px-2 py-1 duration-150 hover:bg-indigo-100"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                />
-              </svg>
-              {cartQuantity ? (
-                <span className="absolute top-0 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 p-1 text-xs text-red-50">
-                  {cartQuantity}
-                </span>
-              ) : null}
-            </Link>
           </div>
 
           {/* Mobile buttons */}
