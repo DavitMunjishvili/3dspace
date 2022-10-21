e24e823e59536c541abefa3709001b49c507ece3
Drummer3 <drummerthree@gmail.com>
2022-10-21 11:28:21 +0400

    add comma

--- app/routes/admin/products/new.tsx
+++ app/routes/admin/products/new.tsx
@@ -64,7 +64,7 @@ export default function New() {
   useEffect(() => {
     if (!isProcessing) {
       formRef.current?.reset();
-      setProductImages([])
+      setProductImages([]);
     }
   }, [isProcessing]);
 
