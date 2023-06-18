97bb0cd41de4e851582307da892e1196fbe95fe7
Drummer3 <drummerthree@gmail.com>
2023-06-18 18:37:09 +0400

    show user object on admin

--- app/routes/admin/users.tsx
+++ app/routes/admin/users.tsx
@@ -1,3 +1,12 @@
+import { json, LoaderArgs } from "@remix-run/server-runtime";
+import { getUser } from "~/session.server";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ request }: LoaderArgs) {
+  const user = await getUser(request);
+  return json(user);
+}
 export default function AdminUsers() {
-  return <p className="bg-red-500 p-4">Users</p>;
+  const user = useLoaderData<typeof loader>();
+  return <pre className="bg-red-500 p-4">{JSON.stringify(user, null, " ")}</pre>;
 }
