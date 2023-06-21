d190b1e99fd11ceb74ab7fca72d02d621d4daa38
Drummer3 <drummerthree@gmail.com>
2023-06-22 02:06:26 +0400

    add secrets page

---
+++ app/routes/admin/secrets.tsx
@@ -0,0 +1,21 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export function loader() {
+  return json({
+    DATABASE_URL: process.env.DATABASE_URL,
+    SESSION_SECRET: process.env.SESSION_SECRET
+  });
+}
+
+export default function Secrets() {
+  const env = useLoaderData<typeof loader>();
+  return (
+    <div>
+      <ul>
+        <li><span className="font-bold">DATABASE_URL:</span> <code>{env.DATABASE_URL}</code></li>
+        <li><span className="font-bold">SESSION_SECRET:</span> <code>{env.SESSION_SECRET}</code></li>
+      </ul>
+    </div>
+  );
+}
