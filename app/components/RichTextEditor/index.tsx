abd3e20034190d09e2d25d39a0668c480634fa15
Drummer3 <drummerthree@gmail.com>
2023-06-28 23:07:09 +0400

    improve rich text editor

--- app/components/RichTextEditor/index.tsx
+++ app/components/RichTextEditor/index.tsx
@@ -1,7 +1,6 @@
-import { Color } from "@tiptap/extension-color";
-import ListItem from "@tiptap/extension-list-item";
-import TextStyle from "@tiptap/extension-text-style";
 import { EditorContent, useEditor } from "@tiptap/react";
+import { Color } from "@tiptap/extension-color";
+import TextStyle from "@tiptap/extension-text-style";
 import StarterKit from "@tiptap/starter-kit";
 import MenuBar from "./MenuBar";
 
@@ -16,20 +15,7 @@ export default function RichTextEditor({
 }) {
   const editor = useEditor(
     {
-      extensions: [
-        Color.configure({ types: [TextStyle.name, ListItem.name] }),
-        TextStyle,
-        StarterKit.configure({
-          bulletList: {
-            keepMarks: true,
-            keepAttributes: false,
-          },
-          orderedList: {
-            keepMarks: true,
-            keepAttributes: false,
-          },
-        }),
-      ],
+      extensions: [Color, TextStyle, StarterKit],
       editable: !viewer,
       content: content,
     },
