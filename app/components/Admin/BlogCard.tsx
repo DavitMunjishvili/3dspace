e24416a9ae99dc9ee3dd4d4df66c098190c3a989
Drummer3 <drummerthree@gmail.com>
2023-06-29 09:18:59 +0400

    add nonfunctional delete button for devs only

--- app/components/Admin/BlogCard.tsx
+++ app/components/Admin/BlogCard.tsx
@@ -1,4 +1,7 @@
 import RichTextEditor from "~/components/RichTextEditor";
+import { useOptionalUser } from "~/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 type Blog = {
   id: number;
@@ -8,8 +11,14 @@ type Blog = {
   views: number;
 };
 export function BlogCard({ blog }: { blog: Blog }) {
+
+  const user = useOptionalUser()
+
   return (
-    <div className="max-h-96 w-full overflow-y-auto rounded bg-violet-50 p-4 shadow-lg">
+    <div className="relative max-h-96 w-full overflow-y-auto rounded bg-violet-50 p-4 shadow-lg">
+      {user?.role === 'dev' && <button className="flex justify-center items-center bg-red-500 w-6 h-6 rounded text-red-50 absolute top-4 right-4">
+        <FontAwesomeIcon size="sm" icon={faTrash} />
+      </button>}
       <h1 className="font-semibild flex items-center justify-center gap-2 text-2xl">
         {blog.title}
         <span className="text-base font-normal text-gray-400">
