4447ba0862a31910c193adc342ff2a5253712642
Drummer3 <drummerthree@gmail.com>
2023-06-29 09:19:22 +0400

    format

--- app/components/Admin/BlogCard.tsx
+++ app/components/Admin/BlogCard.tsx
@@ -11,14 +11,15 @@ type Blog = {
   views: number;
 };
 export function BlogCard({ blog }: { blog: Blog }) {
-
-  const user = useOptionalUser()
+  const user = useOptionalUser();
 
   return (
     <div className="relative max-h-96 w-full overflow-y-auto rounded bg-violet-50 p-4 shadow-lg">
-      {user?.role === 'dev' && <button className="flex justify-center items-center bg-red-500 w-6 h-6 rounded text-red-50 absolute top-4 right-4">
-        <FontAwesomeIcon size="sm" icon={faTrash} />
-      </button>}
+      {user?.role === "dev" && (
+        <button className="absolute right-4 top-4 flex h-6 w-6 items-center justify-center rounded bg-red-500 text-red-50">
+          <FontAwesomeIcon size="sm" icon={faTrash} />
+        </button>
+      )}
       <h1 className="font-semibild flex items-center justify-center gap-2 text-2xl">
         {blog.title}
         <span className="text-base font-normal text-gray-400">
